68e31a416f901186f2ac45c2eab410268ebd7175
mohamed ahmed <anamohamedahmed893@gmail.com>
2024-03-23 14:15:26 +0200

    last update in image

--- src/modules/cloudinary/cloudinary.js
+++ src/modules/cloudinary/cloudinary.js
@@ -1,11 +1,11 @@
 
 
 import {v2 as cloudinary} from 'cloudinary';
-          
+                 
 cloudinary.config({ 
-  cloud_name: process.env.CLOUD_NAME, 
-  api_key: process.env.CLOUD_API_KEY, 
-  api_secret: process.env.CLOUD_SECRET ,
+  cloud_name: "dkfbkijyf", 
+  api_key: '391121483247773', 
+  api_secret: 'CgHv2mB6PAz1ny2EFXLqMkd6hU8' ,
   secure: true
 });
 
